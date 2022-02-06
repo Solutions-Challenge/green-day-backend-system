9298c18e797b8075ca208ff35ae9fa05f366a85b
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-02-05 20:01:30 -0800

    changed data.py

--- data.py
+++ data.py
@@ -1,82 +1,3 @@
-data = {
-    "Picture of a Wooden Object": {
-        "Material": "Wood",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of a Metallic Object": {
-        "Material": "Metal",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of an Electronic device": {
-        "Material": "Electronic",
-        "Recyclability": "Special-Case"
-    },
-    "Picture of a Human": {
-        "Material": "Human",
-        "Recyclability": "Special-Case"
-    },
-    "Picture of Rubber or Latex Gloves": {
-        "Material": "Rubber",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Styrofoam": {
-        "Material": "Styrofoam",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Plastic Bag": {
-        "Material": "Plastic Bag",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of a Plastic Wrapper or Plastic Film": {
-        "Material": "Plastic Wrapper or Film",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Bubble Wrap": {
-        "Material": "Bubble Wrap",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Shredded Paper": {
-        "Material": "Shredded Paper",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of Soiled Paper": {
-        "Material": "Soiled Paper",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Clean Paper": {
-        "Material": "Clean Paper",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of Broken Glass": {
-        "Material": "Broken Glass",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Ceramic": {
-        "Material": "Ceramic",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of Glassware": {
-        "Material": "Glassware",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of Cardboard which doesn't contain food": {
-        "Material": "Cardboard",
-        "Recyclability": "Recyclable"
-    },
-    "Picture of a Cardboard which contains pizza": {
-        "Material": "Pizza Box",
-        "Recyclability": "Not Recyclable"
-    },
-    "Picture of an Animal": {
-        "Material": "Animal",
-        "Recyclability": "Special-Case"
-    },
-    "Picture of a Plant": {
-        "Material": "Plant",
-        "Recyclability": "Bio-degradable"
-    }
-}
-
 mapData = [
     {
         "key": 1,
@@ -110,3 +31,101 @@ mapData = [
     },
 
 ]
+
+data = {
+    "Picture of a Wooden Object": {
+        "Material": "Wood",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[0]
+    },
+    "Picture of a Metallic Object": {
+        "Material": "Metal",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[1]
+    },
+    "Picture of an Electronic device": {
+        "Material": "Electronic",
+        "Recyclability": "Special-Case",
+        "mapData": mapData[5]
+    },
+    "Picture of a Human": {
+        "Material": "Human",
+        "Recyclability": "Special-Case",
+        "mapData": mapData[0]
+    },
+    "Picture of Rubber or Latex Gloves": {
+        "Material": "Rubber",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[0]
+    },
+    "Picture of Styrofoam": {
+        "Material": "Styrofoam",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[3]
+    },
+    "Picture of Plastic Bag": {
+        "Material": "Plastic Bag",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[3]
+    },
+    "Picture of a Plastic Wrapper or Plastic Film": {
+        "Material": "Plastic Wrapper or Film",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[3]
+    },
+    "Picture of Bubble Wrap": {
+        "Material": "Bubble Wrap",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[3]
+    },
+    "Picture of Shredded Paper": {
+        "Material": "Shredded Paper",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[4]
+    },
+    "Picture of Soiled Paper": {
+        "Material": "Soiled Paper",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[4]
+    },
+    "Picture of Clean Paper": {
+        "Material": "Clean Paper",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[4]
+    },
+    "Picture of Broken Glass": {
+        "Material": "Broken Glass",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[2]
+    },
+    "Picture of Ceramic": {
+        "Material": "Ceramic",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[2]
+    },
+    "Picture of Glassware": {
+        "Material": "Glassware",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[2]
+    },
+    "Picture of Cardboard which doesn't contain food": {
+        "Material": "Cardboard",
+        "Recyclability": "Recyclable",
+        "mapData": mapData[0]
+    },
+    "Picture of a Cardboard which contains pizza": {
+        "Material": "Pizza Box",
+        "Recyclability": "Not Recyclable",
+        "mapData": mapData[0]
+    },
+    "Picture of an Animal": {
+        "Material": "Animal",
+        "Recyclability": "Special-Case",
+        "mapData": mapData[0]
+    },
+    "Picture of a Plant": {
+        "Material": "Plant",
+        "Recyclability": "Bio-degradable",
+        "mapData": mapData[0]
+    }
+}
