b9d9be8935edc4e6c2abe0a7c54c10b864007747
Albert Lee <allilee@ucsc.edu>
2022-02-25 15:39:29 -0800

    Changed init_app to blank in auth_server

--- auth_server.py
+++ auth_server.py
@@ -3,7 +3,6 @@ from firebase_admin import credentials, firestore, auth
 import googlemaps
 
 gmaps = googlemaps.Client(key="AIzaSyAVR75_hunpkE3V1kbcJXfKHt1D2B3YgQs")
-cred = credentials.Certificate("greenday-6aba2-firebase-adminsdk-wppee-88cc844ed3.json")
-admin = firebase_admin.initialize_app(cred)
+admin = firebase_admin.initialize_app()
 db = firestore.client()
 
