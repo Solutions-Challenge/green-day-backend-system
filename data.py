6025e34f8b6ea3fc458705727037f68f72818d5b
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-03-21 21:49:25 -0700

    changed to google ml vision

--- data.py
+++ data.py
@@ -32,99 +32,134 @@ mapData = [
 ]
 
 data = {
-    "Picture of a Wooden Object": {
-        "Material": "Wood",
-        "Recyclability": "Recyclable",
+    "aerosol": {
+        "info": "Aerosol containers are generally made of steel, which is easily recycled; however, full or partially-full aerosol containers cannot be placed at the curb because they are under pressure and may pose a hazard to solid waste workers and others.",
         "mapData": mapData[0]
     },
-    "Picture of a Metallic Object": {
-        "Material": "Metal",
-        "Recyclability": "Recyclable",
+    "aluminium_foil": {
+        "info": "If you can't separate the foil from other material such as in drink boxes, candy wrappers, or yogurt tops, throw to the landfill instead.",
         "mapData": mapData[0]
     },
-    "Picture of an Electronic device": {
-        "Material": "Electronic",
-        "Recyclability": "Special-Case",
-        "mapData": mapData[3]
-    },
-    "Picture of a Human": {
-        "Material": "Human",
-        "Recyclability": "Special-Case",
-        "mapData": mapData[1]
-    },
-    "Picture of Rubber or Latex Gloves": {
-        "Material": "Rubber",
-        "Recyclability": "Not Recyclable",
+    "broken_glass": {
+        "info": "Check your municipality's specific rules and requirements.",
         "mapData": mapData[2]
     },
-    "Picture of Styrofoam": {
-        "Material": "Styrofoam",
-        "Recyclability": "Not Recyclable",
+    "cigarette": {
+        "info": "E-cigarettes could be recycled as an electronic component. Empty packs are recyclable.",
         "mapData": mapData[2]
     },
-    "Picture of Plastic Bag": {
-        "Material": "Plastic Bag",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of a Plastic Wrapper or Plastic Film": {
-        "Material": "Plastic Wrapper or Film",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of Bubble Wrap": {
-        "Material": "Bubble Wrap",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of Shredded Paper": {
-        "Material": "Shredded Paper",
-        "Recyclability": "Recyclable",
+    "clear_plastic_bottle": {
+        "info": "Empty bottled water containers (including the caps) should always be placed in a recycle bin.",
         "mapData": mapData[0]
     },
-    "Picture of Soiled Paper": {
-        "Material": "Soiled Paper",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of Clean Paper": {
-        "Material": "Clean Paper",
-        "Recyclability": "Recyclable",
-        "mapData": mapData[0]
-    },
-    "Picture of Broken Glass": {
-        "Material": "Broken Glass",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of Ceramic": {
-        "Material": "Ceramic",
-        "Recyclability": "Not Recyclable",
-        "mapData": mapData[2]
-    },
-    "Picture of Glassware": {
-        "Material": "Glassware",
-        "Recyclability": "Recyclable",
-        "mapData": mapData[5]
-    },
-    "Picture of Cardboard which doesn't contain food": {
-        "Material": "Cardboard",
-        "Recyclability": "Recyclable",
+    "corrugated_carton": {
+        "info": "Typically can also be placed in recycling bins.",
         "mapData": mapData[4]
     },
-    "Picture of a Cardboard which contains pizza": {
-        "Material": "Pizza Box",
-        "Recyclability": "Not Recyclable",
+    "crisp_packet": {
+        "info": "Typically filled with aluminum laminated with polypropylene, which can't be separated easily.",
         "mapData": mapData[2]
     },
-    "Picture of an Animal": {
-        "Material": "Animal",
-        "Recyclability": "Special-Case",
+    "disposable_food_container": {
+        "mapData": mapData[2]
+    },
+    "disposable_plastic_cup": {
+        "mapData": mapData[2]
+    },
+    "drink_can": {
+        "info": "Make sure to clean the item before recycling.",
+        "mapData": mapData[0]
+    },
+    "drink_carton": {
+        "mapData": mapData[2]
+    },
+    "egg_carton": {
+        "mapData": mapData[4]
+    },
+    "foam_cup": {
+        "mapData": mapData[2]
+    },
+    "foam_food_container": {
+        "mapData": mapData[2]
+    },
+    "food_can": {
+        "mapData": mapData[0]
+    },
+    "garbage_bag": {
+        "mapData": mapData[2]
+    },
+    "glass_bottle": {
+        "mapData": mapData[5]
+    },
+    "metal_carton": {
+        "mapData": mapData[0]
+    },
+    "metal_bottle_cap": {
+        "mapData": mapData[0]
+    },
+    "other_carton": {
+        "mapData": mapData[0]
+    },
+    "other_plastic": {
+        "mapData": mapData[2]
+    },
+    "other_plastic_bottle": {
+        "mapData": mapData[2]
+    },
+    "other_plastic_wrapper": {
+        "mapData": mapData[2]
+    },
+    "paper_bag": {
         "mapData": mapData[1]
     },
-    "Picture of a Plant": {
-        "Material": "Plant",
-        "Recyclability": "Bio-degradable",
+    "paper_cup": {
+        "info": "Paper cups are typically coated with plastic, making it difficult to recycle.",
+        "mapData": mapData[2]
+    },
+    "plastic_bottle_cap": {
+        "info": "Bottle caps typically get lost during the recycling process.",
+        "mapData": mapData[0]
+    },
+    "plastic_film": {
+        "mapData": mapData[2]
+    },
+    "plastic_lid": {
+        "mapData": mapData[0]
+    },
+    "plastic_straw": {
+        "mapData": mapData[2]
+    },
+    "plastic_utensils": {
+        "mapData": mapData[2]
+    },
+    "pop_tab": {
+        "mapData": mapData[0]
+    },
+    "rope_&_strings": {
         "mapData": mapData[1]
-    }
+    },
+    "scrap_metal": {
+        "info": "You typically need special recycling bins for this material.",
+        "mapData": mapData[0]
+    },
+    "single-use_carrier_bag": {
+        "mapData": mapData[2]
+    },
+    "styrofoam_piece": {
+        "mapData": mapData[2]
+    },
+    "tissues": {
+        "mapData": mapData[1]
+    },
+    "other_carton": {
+        "mapData": mapData[0]
+    },
+    "unlabeled_litter": {
+        "mapData": mapData[2]
+    },
+    "wrapping_paper": {
+        "info": "Make sure it doesn't contain any metallic flakes, colored shapes, glitter, or plastics.",
+        "mapData": mapData[0]
+    },
+    
 }
